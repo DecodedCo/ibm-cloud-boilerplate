172ba52b386232bf00fdb4d9959978db9bf5e55f
Amadeus Stevenson <amadeus@decoded.com>
2018-01-09 15:22:17 -0500

    Initial AppID integration

--- server.js
+++ server.js
@@ -1,5 +1,32 @@
-var express = require("express"),
-    app = express();
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const WebAppStrategy = require("bluemix-appid").WebAppStrategy;
+const app = express();
+const CALLBACK_URL = "/ibm/bluemix/appid/callback";
+app.use(session({
+  secret: "c85320d9ddb90c13f4",
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new WebAppStrategy());
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+app.get(CALLBACK_URL, passport.authenticate(WebAppStrategy.STRATEGY_NAME));
+app.get("/protected", passport.authenticate(WebAppStrategy.STRATEGY_NAME), function(req, res) {
+  res.json(req.user);
+});
 
 var port = process.env.PORT || 8080;
 
